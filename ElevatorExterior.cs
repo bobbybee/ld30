15037f463e0c91e00a3392e4021a295f81675839
bobbybee <j104@rocketmail.com>
2014-08-23 15:40:00 -0400

    Elevator exterior

---
+++ ElevatorExterior.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorExterior : MonoBehaviour {
+	public string elevatorName = "PurgToHell";
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Application.LoadLevel("elevator"+elevatorName);
+	}
+}
