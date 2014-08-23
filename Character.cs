37cbb988053ce688615e6ae98b49013ceed797e4
bobbybee <j104@rocketmail.com>
2014-08-22 21:54:05 -0400

    Movement

---
+++ Character.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public float movementSpeed = 0.2f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+		if(movementVector.magnitude > 0.2) {
+			transform.position = (Vector2) transform.position + (movementVector * movementSpeed);
+		}
+	}
+
+}
