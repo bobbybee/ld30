fbad3b118ea694052a4445d0cda7971df1d2d8e6
bobbybee <j104@rocketmail.com>
2014-08-23 09:56:19 -0400

    Seperate open and closed characters; disable vertical movement on open characters

--- ClosedCharacter.cs
+++ ClosedCharacter.cs
@@ -1,20 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OpenCharacter : MonoBehaviour {
-
-	public float movementSpeed = 0.1f;
-
-	void Start () {
+public class ClosedCharacter : MonoBehaviour {
 	
+	public float movementSpeed = 0.1f;
+	
+	void Start () {
+		
 	}
 	
 	void Update () {
-		Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), 0);
-
+		Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		
 		if(movementVector.magnitude > 0.2) {
 			transform.position = (Vector2) transform.position + (movementVector * movementSpeed);
 		}
 	}
-
+	
 }
