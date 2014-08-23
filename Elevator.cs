688a264be7899f32394cbfb1496f3cf73fc80b82
bobbybee <j104@rocketmail.com>
2014-08-23 15:23:17 -0400

    Elevator framework

---
+++ Elevator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+	public string destination = "hell";
+	
+	void Update () {
+		if(!audio.isPlaying) {
+			Application.LoadLevel(destination);
+		}
+	}
+}
