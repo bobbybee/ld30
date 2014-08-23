5c171f6a3973fea88dfc4c629c8e10998d940d34
bobbybee <j104@rocketmail.com>
2014-08-23 17:39:29 -0400

    Working keypad

--- Keypad.cs
+++ Keypad.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Keypad : MonoBehaviour {
+
+	public GUIStyle inputStyle;
+
+	List<int> numbers = new List<int>();
 
 	// Use this for initialization
 	void Start () {
@@ -15,15 +21,38 @@ public class Keypad : MonoBehaviour {
 
 	void OnGUI() {
 
-		float buttonWidth = Screen.width / 4;
-		float buttonHeight = Screen.height / 4;
+		GUI.Label(new Rect(0, 0, Screen.width, Screen.height / 5), string.Join("", numbers.Select(x=>x.ToString()).ToArray()), inputStyle);
 
-		string[] buttonLabels = new string[]{"1","2","3","4","5","6","7","8","9","#","0","ENTER"};
+		float buttonWidth = Screen.width / 3;
+		float buttonHeight = Screen.height / 5;
+
+		string[] buttonLabels = new string[]{"1","2","3","4","5","6","7","8","9","BACKSPACE","0","ENTER"};
 
 		for(int i = 0; i < 4; ++i) {
-			for(int j = 0; j < 4; ++j) {
-				if(GUI.Button(new Rect(j * buttonWidth, i * buttonHeight, buttonWidth, buttonHeight), buttonLabels[(i*4)+j])) {
-					Debug.Log ( (i + 4) + j);
+			for(int j = 0; j < 3; ++j) {
+				if(GUI.Button(new Rect(j * buttonWidth, (i+1) * buttonHeight, buttonWidth, buttonHeight), buttonLabels[(i*3)+j])) {
+					int index = (i * 3) + j;
+
+					if(index < 9) {
+						numbers.Add (index + 1);
+					} else if (index == 10) {
+						numbers.Add (0);
+					} else if(index == 9) {
+						numbers.Remove(numbers.Last());
+					} else if(index == 11) {
+						int trying = 0;
+						int powIndex = 0;
+
+						for(int numIndex = numbers.Count - 1; numIndex >= 0; --numIndex) {
+							trying += numbers[numIndex] * (int) Mathf.Pow(10, (float) powIndex);
+
+							powIndex++;
+						}
+
+						Debug.Log (trying);
+					} else {
+						Debug.Log (buttonLabels[index]);
+					}
 				}
 			}
 		}
