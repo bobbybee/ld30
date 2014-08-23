2d582c15090ccc22a22ee74ac84fa1887873ce20
bobbybee <j104@rocketmail.com>
2014-08-23 14:29:49 -0400

    Guard

---
+++ Guard.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Guard : MonoBehaviour {
+
+	public Rect keyLocation;
+	public AudioSource taunt;
+	public AudioSource master;
+
+	public int grabKeyTime = 0;
+
+	public Sprite portalSprite;
+
+	private bool keyGrabbed = false;
+	private bool isPortal = false;
+
+	void Update () {
+		if(keyGrabbed && !master.isPlaying) {
+			keyGrabbed = false;
+			isPortal = true;
+
+			(renderer as SpriteRenderer).sprite = portalSprite;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if(!keyGrabbed && !isPortal) {
+			taunt.Play();
+			grabKeyTime = 90;
+		} else if(isPortal) {
+			Application.LoadLevel("control room");
+		}
+	}
+
+	void OnGUI() {
+		if(grabKeyTime > 0) {
+			grabKeyTime--;
+
+			if(GUI.Button(keyLocation, "Grab key")) {
+				taunt.Stop();
+
+				keyGrabbed = true;
+
+				master.Play();
+			}
+		}
+	}
+}
