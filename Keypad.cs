c4cef3e24ae5b549946aa2403e589418d3f94263
bobbybee <j104@rocketmail.com>
2014-08-23 17:11:01 -0400

    Crude keypad

---
+++ Keypad.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keypad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+
+		float buttonWidth = Screen.width / 4;
+		float buttonHeight = Screen.height / 4;
+
+		string[] buttonLabels = new string[]{"1","2","3","4","5","6","7","8","9","#","0","ENTER"};
+
+		for(int i = 0; i < 4; ++i) {
+			for(int j = 0; j < 4; ++j) {
+				if(GUI.Button(new Rect(j * buttonWidth, i * buttonHeight, buttonWidth, buttonHeight), buttonLabels[(i*4)+j])) {
+					Debug.Log ( (i + 4) + j);
+				}
+			}
+		}
+
+	}
+}
