67a513eb7471d855bff8b6897a5a5e39194c1534
bobbybee <j104@rocketmail.com>
2014-08-22 22:31:35 -0400

    Working shell (sorta)

---
+++ CMD.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CMD : MonoBehaviour {
+
+	public Rect promptLocation;
+	public string prompt = "> ";
+
+	public Rect cmdPrompt;
+	public Rect enterButton;
+	private string command = "";
+
+	public Rect outputBox;
+	private string output = "";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		GUI.Label(promptLocation, prompt);
+
+		command = GUI.TextField(cmdPrompt, command);
+
+		if(GUI.Button(enterButton, "ENTER") || (Event.current.type == EventType.KeyUp && Event.current.keyCode == KeyCode.Return) ) {
+			output = prompt + command + "\n" + EvaluateCommand(command) + "\n" + output;
+			command = "";
+		}
+
+		GUI.TextField(outputBox, output);
+	}
+
+	string EvaluateCommand(string command) {
+		string[] parts = command.Split (' ');
+
+		return parts[0]+" not found";
+	}
+}
