ac40b0fb4f1490daf9297472174572ef5ff503f3
bobbybee <j104@rocketmail.com>
2014-08-24 09:29:57 -0400

    Automatic full screen in webplayer to fix focus bug

---
+++ goFullScreen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goFullScreen : MonoBehaviour {
+	void Start() {
+		if(Application.isWebPlayer) {
+			Screen.fullScreen = true;
+		}
+	}
+}
